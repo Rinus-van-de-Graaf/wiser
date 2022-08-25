619e88286b3f7704ec2ec2d4cb0531a3711ab2c1
Hans <35447467+HansJuice@users.noreply.github.com>
2022-08-25 17:09:20 +0200

    Changes for MySQL document store
    Required changes for the changes in the GCL to support the document store.

--- Api/Core/Queries/WiserInstallation/CreateTables.sql
+++ Api/Core/Queries/WiserInstallation/CreateTables.sql
@@ -48,6 +48,7 @@ CREATE TABLE IF NOT EXISTS `wiser_entity`  (
   `enable_multiple_environments` tinyint(1) NOT NULL DEFAULT 0 COMMENT 'Whether or not to enable multiple environments for entities of this type. This means that the test can have a different version of an item than the live for example.',
   `dedicated_table_prefix` varchar(25) NOT NULL DEFAULT '',
   `delete_action` enum('archive','permanent','hide','disallow') CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT 'archive',
+  `store_type` enum('normal','document_store','hybrid') CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT 'normal',
   PRIMARY KEY (`id`) USING BTREE,
   UNIQUE INDEX `name_module_id`(`name`, `module_id`) USING BTREE,
   INDEX `name`(`name`(100), `show_in_tree_view`) USING BTREE,
